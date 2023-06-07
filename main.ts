5ff189fa03ebe56b15aa5d90eceb37d6d2d2b75b
Matt Hale <mlhale@unomaha.edu>
2023-06-07 16:06:10 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    control.waitMicros(60000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    control.waitMicros(60000)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+})
+basic.showLeds(`
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    . . . . #
+    `)
